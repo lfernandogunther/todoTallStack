8ea70ef99acd6f13625c3f2d83e2a5c8608b9692
Fernando Gunther <lfernando.gunther@devsquad.com>
2022-11-09 18:14:29 -0300

    init
    - create todo model
    - create factory and seeder todo
    - create init livewire component
    - import initial libs

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TodoFactory extends Factory
+{
+
+    public function definition()
+    {
+        return [
+            'description' => $this->faker->text()
+        ];
+    }
+}
---
+++ database/seeders/TodoSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Todo;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TodoSeeder extends Seeder
+{
+    public function run()
+    {
+        Todo::factory()->count(10)->create();
+    }
+}
