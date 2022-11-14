0236b6a98b49250f0c5d93b49d0be0c595ce4f54
Fernando Gunther <lfernando.gunther@devsquad.com>
2022-11-14 13:28:05 -0300

    fix test, change actions delete button label

--- resources/views/livewire/todo/actions.blade.php
+++ resources/views/livewire/todo/actions.blade.php
@@ -1,4 +1,11 @@
 <div class="flex
-    justify-end items-start gap-2 py-2 px-4">
-    <x-button :disabled="$this->isTodoListEmpty" label="Reset" wire:click='confirmReset' red icon="trash" />
+    justify-end items-start gap-2 py-2">
+    <x-button
+        :disabled="$this->isTodoListEmpty"
+        label="Delete All"
+        wire:click='confirmReset'
+        white
+        flat
+        icon="trash"
+    />
 </div>
--- resources/views/livewire/todo/index.blade.php
+++ resources/views/livewire/todo/index.blade.php
@@ -1,6 +1,9 @@
-<div x-data="{
-    hover: null
-}" class="flex justify-center min-h-screen w-full  bg-teal-600">
+<div
+    x-data="{
+        hover: null
+    }"
+    class="flex justify-center min-h-screen w-full  bg-teal-600"
+>
     <div class="w-full p-4 md:max-w-2xl">
         <h3 class='font-semibold text-2xl text-white w-full text-center mb-4'>
             List of things to do
@@ -17,23 +20,42 @@
 
         <ul class="space-y-4">
             @foreach ($todoList as $todo)
-                <li wire:key="list:{{ $todo->id }}" @class([
-                    'bg-gray-100' => !$todo->done,
-                    'bg-blue-200' => $todo->done,
-                    'body-hidden flex justify-between items-center gap-2 p-4 shadow-lg rounded-lg',
-                ])>
-                    <livewire:todo.toggle wire:key="toggle:{{ $todo->id }}" :todo="$todo" />
+                <li
+                    wire:key="list:{{ $todo->id }}"
+                    @class([
+                        'bg-gray-100' => !$todo->done,
+                        'bg-blue-200' => $todo->done,
+                        'body-hidden flex justify-between items-center gap-2 p-4 shadow-lg rounded-lg',
+                    ])
+                >
+                    <livewire:todo.toggle
+                        wire:key="toggle:{{ $todo->id }}"
+                        :todo="$todo"
+                    />
 
-                    <label for="toggle.{{ $todo->id }}" @class(['line-through' => $todo->done, 'w-full cursor-pointer'])>
+                    <label
+                        for="toggle.{{ $todo->id }}"
+                        @class(['line-through' => $todo->done, 'w-full cursor-pointer'])
+                    >
                         {{ $todo->description }}
                     </label>
 
                     <div class="flex-table-hidden-actions flex justify-between gap-2">
                         @if (!$todo->done)
-                            <x-button wire:click="edit('{{ $todo->id }}')" rounded flat primary icon="pencil" />
+                            <x-button
+                                id="todo.edit:{{ $todo->id }}"
+                                wire:click="edit('{{ $todo->id }}')"
+                                rounded
+                                flat
+                                primary
+                                icon="pencil"
+                            />
                         @endif
 
-                        <livewire:todo.delete wire:key="delete:{{ $todo->id }}" :todo="$todo" />
+                        <livewire:todo.delete
+                            wire:key="delete:{{ $todo->id }}"
+                            :todo="$todo"
+                        />
                     </div>
                 </li>
             @endforeach
